e06cfd13a8b524eb5e73c8ddc0b118baa2a0bd51
Tiago Valdrich <tiago.valdrich@hotmail.com>
2021-06-16 11:30:45 -0300

    small fixes for docker-compose

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 const database = require("./database");
 const app = require("./app");
 
+const sleep = (time) => {
+  return new Promise((resolve) => setTimeout(resolve, time));
+};
+
 (async () => {
   try {
+    // This is for docker-compose don't break
+    if (process.env.DATABASE_DELAY) {
+      await sleep(process.env.DATABASE_DELAY);
+    }
+
     await database.createInstance();
 
     app.listen("3000", () => {
